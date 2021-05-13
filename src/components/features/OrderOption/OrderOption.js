c1efaa2f925bfd72bc19d03d6d1c576f7d969519
Marta Nitek <marta.nitek@onet.pl>
2021-05-13 23:59:07 +0200

    Wyświetlanie Pojedynczej Opcji

---
+++ src/components/features/OrderOption/OrderOption.js
@@ -0,0 +1,15 @@
+import styles from './OrderOption.scss';
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const OrderOption = ({name}) => (
+  <div className={styles.component}>
+    <h3 className={styles.title}>{name}</h3>
+  </div>
+);
+
+OrderOption.propTypes ={
+  name: PropTypes.string,
+};
+    
+export default OrderOption;
